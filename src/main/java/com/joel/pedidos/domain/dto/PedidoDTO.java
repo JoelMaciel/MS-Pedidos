12b1da50d694970601db93a64eb43e5b0310b114
Joel Maciel <soldado25461@gmail.com>
2023-05-07 09:19:47 -0300

    feat: balanceamento de cargas

--- src/main/java/com/joel/pedidos/domain/dto/PedidoDTO.java
+++ src/main/java/com/joel/pedidos/domain/dto/PedidoDTO.java
@@ -1,12 +1,10 @@
 package com.joel.pedidos.domain.dto;
 
-import com.joel.pedidos.domain.enums.Status;
-import com.sun.istack.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +12,12 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@Builder
 @AllArgsConstructor
 public class PedidoDTO {
 
     private Long id;
-    @NotNull
     private LocalDateTime dataHora;
-
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private Status status;
-
-    @Valid
+    private StatusDTO status;
     private List<ItemDoPedidoDTO> itens = new ArrayList<>();
 
 
